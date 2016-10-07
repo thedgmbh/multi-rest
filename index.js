7c31502e5acd8d4bf4090717fefb471f89e659cd
M Mahrous <m.mahrous@thed.io>
2016-10-07 03:28:56 +0300

    error handling

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ var upload = {};
 var options;
 
 upload = function (req, res, next) {
-	if (!req.files[options.filefield]) res.send({code: "ExternalError", messege: "Cannot read property '" + options.filefield + "' of undefined"})
-	var newPath = options.uploadDir + filename(req.files[options.filefield]) + '.' + fileExtension(req.files[options.filefield].name);
-	fs.move(req.files[options.filefield].path,  newPath , function (err) {
-		if (err) return res.send({code: "InternalError", messege: "Error happen while uploading."})
-		delete req.files[options.filefield].domain;
-		delete req.files[options.filefield]._writeStream;
-		delete req.files[options.filefield]._events;
-		delete req.files[options.filefield]._eventsCount;
-		delete req.files[options.filefield]._maxListeners;
-		req.files[options.filefield].path = newPath;
-		next();
-	})
+	if (typeof req.files[options.filefield] == 'undefined') {
+		if (options.used == "maybe") {
+			next()
+		} else if (options.used == "must") {
+			res.send({code: "ExternalError", messege: "Cannot read property '" + options.filefield + "' of undefined"});
+		}
+	}else {
+		var newPath = options.uploadDir + filename(req.files[options.filefield]) + '.' + fileExtension(req.files[options.filefield].name);
+		fs.move(req.files[options.filefield].path,  newPath , function (err) {
+			if (err) return res.send({code: "InternalError", messege: "Error happen while uploading."})
+			delete req.files[options.filefield].domain;
+			delete req.files[options.filefield]._writeStream;
+			delete req.files[options.filefield]._events;
+			delete req.files[options.filefield]._eventsCount;
+			delete req.files[options.filefield]._maxListeners;
+			req.files[options.filefield].path = newPath;
+			next();
+		})
+	}
 }
 
 upload.options = function (data) {
